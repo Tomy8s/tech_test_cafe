c3faf6faa75b93eab9b91b974afd8c1d8ffe3b1e
Tomy8s <tomy8s@hotmail.com>
2016-12-02 15:54:03 +0000

    Print::print prints total

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,6 +1,12 @@
+require 'order'
+
 class Printer
-  def self.print(cafe_info)
+  def self.print(cafe_info, order)
     self.print_cafe_info(cafe_info)
+    puts #linebreak
+    self.print_order(order)
+    puts #linebreak
+    self.print_total(order)
   end
 
   def self.print_cafe_info(cafe_info)
@@ -9,5 +15,18 @@ class Printer
     puts #linebreak
     puts cafe_info[:address]
     puts cafe_info[:phone]
+  end
+
+  def self.print_order(order)
+    order = order.dup
+    while order.size > 0
+      quantity = order.count(order[0])
+      item = order.delete(order[0])
+      puts item.name + quantity.to_s + ' x ' + item.price.to_s.insert(-3, '.')  + ' = ' + (quantity * item.price).to_s.insert(-3, '.')
+    end
+  end
+
+  def self.print_total(order)
+    puts "total" + order.collect{ |item| item.price }.reduce(:+).to_s.insert(-3, '.')
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -2,15 +2,29 @@ require 'spec_helper'
 
 describe Printer do
   let(:till) { Till.new('My Cafe', '5 High St', '123456789', Menu.new) }
+  let(:coffee) { Item.new('coffee',342) }
+  let(:tea) { Item.new('tea',242) }
+  let(:order) { Order.new }
+
+  before do
+    order.order_so_far << coffee
+    5.times{ order.order_so_far << tea }
+  end
+
   describe'#print' do
-    it 'prints the date and time' do
-      expect{ subject.class.print(name: 'My Cafe', address: '5 High St', phone: '123456789') }.to output(
+    it 'prints the relevant information' do
+      expect{ subject.class.print({name: 'My Cafe', address: '5 High St', phone: '123456789'}, order.order_so_far) }.to output(
         "\
 #{Time.now.strftime('%Y.%m.%d %H:%M:%S')}
 My Cafe
 
 5 High St
 123456789
+
+coffee1 x 3.42 = 3.42
+tea5 x 2.42 = 12.10
+
+total15.52
 "
         ).to_stdout
     end
