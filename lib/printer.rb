80abd3344ff2214628c1b71afaa6c67a70d35cce
Tomy8s <tomy8s@hotmail.com>
2016-12-02 14:08:55 +0000

    Printer::print prints cafe details

--- lib/printer.rb
+++ lib/printer.rb
@@ -1,5 +1,8 @@
 class Printer
-  def self.print
+  def self.print(cafe_info)
     puts Time.now.strftime('%Y.%m.%d %H:%M:%S')
+    puts cafe_info[:name]
+    puts cafe_info[:address]
+    puts cafe_info[:phone]
   end
 end
--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe Printer do
+  let(:till) { Till.new('My Cafe', '5 High St', '123456789', Menu.new) }
   describe'#print' do
     it 'prints the date and time' do
-      expect{ subject.class.print }.to output(
-        "#{Time.now.strftime('%Y.%m.%d %H:%M:%S')}\n"
+      expect{ subject.class.print(name: 'My Cafe', address: '5 High St', phone: '123456789') }.to output(
+        "\
+#{Time.now.strftime('%Y.%m.%d %H:%M:%S')}
+My Cafe
+5 High St
+123456789
+"
         ).to_stdout
     end
   end
