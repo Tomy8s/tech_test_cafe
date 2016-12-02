6662ee4ca1974d694f748db70aa4488c973e35a0
Tomy8s <tomy8s@hotmail.com>
2016-12-02 09:50:43 +0000

    Added tests for Item#price

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Item do
-  let(:subject) { Item.new('coffee') }
+  let(:subject) { Item.new('coffee', 473.0) }
   describe '#name' do
-    it 'is instantiated with a name' do
+    it 'is initiated with a name' do
       expect(subject.name).to eq 'coffee'
     end
 
@@ -17,6 +17,20 @@ describe Item do
       rescue
       end
       expect(subject.name).to eq 'coffee'
+    end
+  end
+
+  describe '#price' do
+    it 'is initiated with a price' do
+      expect(subject.price).to eq 473
+    end
+
+    it 'saves price as an integer' do
+      expect(subject.price).to be_kind_of(Fixnum)
+    end
+
+    it 'can be changed' do
+      expect{ subject.price = 573.0 }.to change(subject.price).to 573
     end
   end
 end
