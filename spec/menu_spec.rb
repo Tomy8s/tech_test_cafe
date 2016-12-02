e0c4745684c556ddb032dbfe8d4f786d23c86b7f
Tomy8s <tomy8s@hotmail.com>
2016-12-02 10:32:02 +0000

    Added tests for Menu#add_item

---
+++ spec/menu_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Menu do
+  describe '#add_item' do
+    it 'adds items to the #items array' do
+      subject.add_item('coffee', 123)
+      expect(subject.items.size).to eq 1
+    end
+
+    it 'raises and error if an item that name already exists' do
+      subject.add_item('coffee', 123)
+      expect{ subject.add_item('coffee', 123) }.to raise_error ArgumentError
+    end
+
+    it 'doesn\'t add an item it an item with that name already exists' do
+      subject.add_item('coffee', 123)
+      begin
+        subject.add_item('coffee', 123)
+      rescue
+      end
+      expect(subject.items.size).to eq 1
+    end
+  end
+end
